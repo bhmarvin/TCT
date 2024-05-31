32177c5cf70bc8ae983df7d5bf6376753121807d
Brent Marvin <brentmarvin@Brents-MacBook-Air-2.local>
2024-05-31 12:26:36 -0400

    added fox and AP

--- Scraper/main.py
+++ Scraper/main.py
@@ -1,8 +1,15 @@
 from models.CNN import CNN
+from models.Fox import Fox
+from models.AP import AP
 import requests
 
 cnn_test = CNN('CNN', 'https://www.cnn.com/', 'https://www.cnn.com/world/middleeast/israel')
 data = cnn_test.fetch_soup()
 links_and_titles = cnn_test.filter_soup(data)
-for link_and_title in links_and_titles:
-    print(link_and_title)
+fox_test = Fox('Fox', 'https://www.foxnews.com/', 'https://www.foxnews.com/category/world/world-regions/israel')
+data = fox_test.fetch_soup()
+links_and_titles = fox_test.filter_soup(data)
+AP_test = AP('AP', 'https://apnews.com/', 'https://apnews.com/hub/israel-hamas-war')
+data = AP_test.fetch_soup()
+links_and_titles = AP_test.filter_soup(data)
+
