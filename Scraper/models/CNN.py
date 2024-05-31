d539faf1a35db1401ba1c4f76a39682705e3ed88
Brent Marvin <brentmarvin@Brents-MacBook-Air-2.local>
2024-05-31 10:33:53 -0400

    gets links and titles from CNN correctly

--- Scraper/models/CNN.py
+++ Scraper/models/CNN.py
@@ -23,12 +23,26 @@ class CNN(Source):
             return None
 
     def filter_soup(self, soup):
-        #card container__item container__item--type-media-image container__item--type-section container_list-headlines-with-images__item container_list-headlines-with-images__item--type-section  
-        #card container__item container__item--type-media-image container__item--type-section container_lead-plus-headlines-with-images__item container_lead-plus-headlines-with-images__item--type-section 
-        #THE TWO CLASSES I NEED TO GET DIVS OF TO GET LINKS/TITLES TOGETHER 
-        pass
+        #maybe mvoe the classes to a DB and fetch from there, can keep updated and such
+        classes = ['card container__item container__item--type-media-image container__item--type-section container_list-headlines-with-images__item container_list-headlines-with-images__item--type-section',
+                   'card container__item container__item--type-media-image container__item--type-section container_lead-plus-headlines-with-images__item container_lead-plus-headlines-with-images__item--type-section'
+                   ]
+        articles = soup.find_all('div', class_=classes)
+        links_and_titles = []
+        for article in articles:
+            link,title = self.get_link_and_title(article)
+            links_and_titles.append((link,title))
+        return links_and_titles
+
 
 
         
     def store_data(self, data):
         pass
+
+    def get_link_and_title(self, article):
+        link = article.find('a').get('href')
+        print(link)
+        title = article.find('span',class_='container__headline-text').get_text(strip=True)
+        print(title)
+        return link,title
